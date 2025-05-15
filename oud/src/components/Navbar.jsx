e2fbadf93060187b3609a715568b6108a46500f1
Huig <153827936+Huig01@users.noreply.github.com>
2025-05-15 13:57:18 +0200

    Navbar alvast aangemaakt

---
+++ oud/src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-yellow-400 border-b border-yellow-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+              <NavLink
+                className="flex flex-shrink-0 items-center mr-4"
+                to="/"
+              >
+                <img
+                  className="h-10 w-auto"
+                  src={logo}
+                  alt="React Jobs"
+                />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                  Pokimon {/* Home */}
+                </span>
+              </NavLink>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink
+                    to="/"
+                    className="text-white bg-red-500 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to="/vrienden"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Vrienden {/*jobs */}
+                  </NavLink>
+                  <NavLink
+                    to="/posts"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Posts {/*jobs */}
+                  </NavLink> 
+                  <NavLink
+                    to="/register"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Register {/* Add Job */}
+                  </NavLink>
+                  <NavLink
+                    to="/login"
+                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >
+                    Login {/* Add Job */}
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
