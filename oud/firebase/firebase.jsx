6266d5473417cc77b21e78102e94a4a07d750913
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-05-21 16:50:18 +0200

    busy with firebase login and register

--- oud/firebase/firebase.jsx
+++ oud/firebase/firebase.jsx
@@ -5,16 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.SENDERID,
-  appId: process.env.APPID
+  apiKey: "AIzaSyCkwq80NycoUy3PPn4S7UmNcZezVZSdqN0",
+  authDomain: "oudbejaard.firebaseapp.com",
+  projectId: "oudbejaard",
+  storageBucket: "oudbejaard.firebasestorage.app",
+  messagingSenderId: "726551290357",
+  appId: "1:726551290357:web:f3bbbba5de6ac6b20a00bc"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// yes
-// yes
+export const db = getFirestore(app);
--- oud/src/pages/Register.jsx
+++ oud/src/pages/Register.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import './Register.css';
 
-const Register = () => {
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { app } from "../../firebase/firebase";
+
+function Registrations() {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [loginError,setLoginError] = useState('');
+
+  const auth = getAuth(app);
+
+  const HandleSignUp =async(e)=>{
+      try{
+          await createUserWithEmailAndPassword(auth,email,password);
+          console.log("Signup Done!")
+      }
+      catch(error){
+        console.log(error);
+      }
+  }
   return (
-    <div>Register</div>
+    <div>
+      <form>
+        <input
+        type="text"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        placeholders="Enter your email..."
+        />
+        <input
+        type="text"
+        value={password}
+        onChange={(e)=>setEmail(e.target.value)}
+        placeholders="Enter your password..."
+        />
+        <button type="submit">Login</button>
+      </form>
+      <button>Sign up</button>
+      <button>Continue with Google</button>
+    </div>
+
   )
 }
-
-export default Register
